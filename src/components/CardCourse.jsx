9f93273f0a97ee8fe7f93917e738172b30e9eee8
Nuthelk <nuthelkcd@gmail.com>
2022-07-22 12:49:44 -0500

    superHero

--- src/components/CardCourse.jsx
+++ src/components/CardCourse.jsx
@@ -1,43 +1,33 @@
-import React, { useEffect, useState } from "react";
+
 import { H3, StyledCard, TextContainer } from "../style/estilos";
-import { urlQuiz } from "../modules/url";
 
-function CardCourse() {
-  const [courseCard, setCourseCard] = useState([]);
 
-  console.log(courseCard);
-  console.log(setCourseCard);
+function CardCourse({img, descripcion, duracion, titulo}) {
+  
 
-  useEffect(() => {
-    const fetchData = async () => {
-      await fetch(urlQuiz)
-      .then((resp) => resp.json())
-      .then((json) => setCourseCard(json))
-      .catch((error) => console.log(error));
-    };
-    fetchData();
-  }, []);
 
-  return (
-    <>
-      {courseCard.map((element) => ( 
-        <StyledCard courses key={element.id}>
-          <div className="card-details-container2">
-            <figure className="card-img-container2">
-              <img src={element.imgen} alt="" className="img2" />
-            </figure>
-            <TextContainer noGap>
-              <H3>{element.titulo}</H3>
-              <p className="p-card">{element.descripcion}</p>
-            </TextContainer>
-          </div>
-          <div className="div-header-cont-figure">
-            <p className="p-card">{`${element.duracion}Min`}</p>
-          </div>
-        </StyledCard>
-      ))}
-    </>
-  );
+ 
+    return (
+      <>
+        
+          <StyledCard >
+            <div className="card-details-container2">
+              <figure className="card-img-container2">
+                <img src={img} alt="" className="img2" />
+              </figure>
+              <TextContainer noGap>
+                <H3>{titulo}</H3>
+                <p className="p-card">{descripcion}</p>
+              </TextContainer>
+            </div>
+            <div className="div-header-cont-figure">
+              <p className="p-card">{`${duracion}Min`}</p>
+            </div>
+          </StyledCard> 
+        
+      </>
+    );
+  
 }
 
 export default CardCourse;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,64 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "../components/Card";
 import CardCourse from "../components/CardCourse";
 import HeaderHome from "../components/MainHome";
+import { getData } from "../helper/CrudSalvavidas";
+import { urlQuiz } from "../modules/url";
 import { P, Section } from "../style/estilos";
 
 
 function Home() {
-  const [nameUser, setNameUser] = useState("");
-  const [coin, setCoin] = useState(0);
+
   const { name, coins } = JSON.parse(sessionStorage.getItem("user"));
-  // const [loading, setLoading] = useState(false)
+  const [courseCard, setCourseCard] = useState(null);
 
-  setCoin(coins);
-  setNameUser(name);
 
-  return (
-    <>
-      <HeaderHome name={nameUser} coins={coin} />
-      <main className="main">
-        {/* Construcción de la sección de scroll o cards */}
-        <P pHome>Courses</P>
-        <div className="scroll">
-          <Section>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+  useEffect(() => {
+    getData(setCourseCard, urlQuiz)
+  }, []);
+
+
+  console.log(name)
+
+  if (courseCard != null) {
+
+    return (
+      <>
+        <HeaderHome name={name} coins={coins} />
+        <main className="main">
+
+          <P pHome>Courses</P>
+          <div className="scroll">
+            <Section>
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+            </Section>
+          </div>
+
+
+
+          <P pHome>Topics to study</P>
+          <Section courses>
+            {courseCard.map((e) =>
+              <CardCourse key={e.id} 
+              img = {e.img}
+              titulo = {e.titulo}
+              descripcion = {e.descripcion}
+              duracion = {e.duracion}
+              
+              />
+              
+              )}
+            
           </Section>
-        </div>
+        </main>
 
-        {/* Sección de los cursos */}
-
-        <P pHome>Topics to study</P>
-        <Section courses>
-            <CardCourse/>
-        </Section>
-      </main>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default Home;
